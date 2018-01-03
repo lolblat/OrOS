b80420e2579e9d9934fd1d51c46bdd3212f859d4
lolblat <orr12orr@gmail.com>
2018-01-03 12:59:17 +0200

    Moved to protected mod wowww added print

---
+++ boot_sector/switch_to_protected.asm
@@ -0,0 +1,12 @@
+;code switch to protected mode
+switch_to_protected:
+	cli ; disable int
+	lgdt[size]
+
+	mov eax, cr0
+	or al, 1 ; switch flag true
+	mov cr0, eax
+
+	jmp 08h:protected_mode ; far jump to code
+	ret
+	leave
